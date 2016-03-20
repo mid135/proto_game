e2b55657182934ca1e522c3cf8af61f1ad4eef9a
mid <moskalukdima@mail.ru>
2016-03-20 23:22:18 +0400

    logout fix

--- proto_game_backend/src/main/java/Main/Main.java
+++ proto_game_backend/src/main/java/Main/Main.java
@@ -6,6 +6,7 @@ import backend.Accounting.AccountServiceImpl;
 import backend.Mechanics.GameMechanics;
 import backend.Mechanics.MyWebSocketHandler;
 import frontend.AccountServlets.Auth;
+import frontend.AccountServlets.LogOut;
 import frontend.AccountServlets.Registration;
 import frontend.WebSocketGameServlet;
 import frontend.WebSocketService;
@@ -22,12 +23,14 @@ public class Main {
 
         Auth auth = new Auth(pool);
         Registration register = new Registration(pool);
+        LogOut logOut = new LogOut(pool);
 
         Server server = new Server(9000);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
         context.addServlet(new ServletHolder(auth), "/auth");
         context.addServlet(new ServletHolder(register),"/register");
+        context.addServlet(new ServletHolder(logOut),"/logout");
 
         WebSocketService webSocketService = new WebSocketService();
         GameMechanics gameMechanics = new GameMechanics(webSocketService,pool);
