6f6f16f77ad8b297c4781a2c5a82ffc518ec117e
mid <moskalukdima@mail.ru>
2016-03-20 23:00:25 +0400

    logout +fixes

--- proto_game_backend/src/main/java/backend/Accounting/AccountServiceImpl.java
+++ proto_game_backend/src/main/java/backend/Accounting/AccountServiceImpl.java
@@ -14,11 +14,13 @@ public class AccountServiceImpl implements AccountService {
     private Map<String, User> arraySessionId = new HashMap<String, User>();//все сессии пользователей - sessionId/UserImplMemory
     private Map<String, User> users = new HashMap<String, User>();//все зарегистрированые юзеры - логин/UserImplMemory
 
-    public Map<String, User> getArraySessionId() {return this.arraySessionId;}
+    public Map<String, User> getArraySessionId() {
+        return this.arraySessionId;
+    }
 
     public AccountEnum logIn(String login, String password, HttpServletRequest request) {
-        if (checkLogIn(request)==AccountEnum.UserNotLoggedIn) {
-            if ( (this.checkRegistration(login) == AccountEnum.UserRegistered) ) {
+        if (checkLogIn(request) == AccountEnum.UserNotLoggedIn) {
+            if ((this.checkRegistration(login) == AccountEnum.UserRegistered)) {
                 if (users.get(login).checkPassword(password)) {
                     arraySessionId.put(request.getSession().getId(), users.get(login));
                     return AccountEnum.LogInSuccess;
@@ -36,8 +38,8 @@ public class AccountServiceImpl implements AccountService {
     public AccountEnum logOut(HttpServletRequest request) {
         if (arraySessionId.containsKey(request.getSession().getId())) {
             User cur = arraySessionId.get(request.getSession().getId());
-            if ( (this.checkRegistration(cur.getLogin()) == AccountEnum.UserRegistered)
-                    && (this.checkLogIn(request) == AccountEnum.UserLoggedIn) ) {
+            if ((this.checkRegistration(cur.getLogin()) == AccountEnum.UserRegistered)
+                    && (this.checkLogIn(request) == AccountEnum.UserLoggedIn)) {
                 arraySessionId.remove(request.getSession().getId());
                 return AccountEnum.LogOffSuccess;
             } else {
@@ -51,7 +53,7 @@ public class AccountServiceImpl implements AccountService {
     public AccountEnum checkRegistration(String userName) {
         if (users.containsKey(userName)) {
             return AccountEnum.UserRegistered;
-        } else  {
+        } else {
             return AccountEnum.UserNotRegistered;
         }
     }
--- proto_game_backend/src/main/java/frontend/AccountServlets/LogOut.java
+++ proto_game_backend/src/main/java/frontend/AccountServlets/LogOut.java
@@ -1,7 +1,39 @@
 package frontend.AccountServlets;
 
+import backend.AccountService;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by moskaluk on 24.02.2016.
  */
-public class LogOut {
+public class LogOut extends HttpServlet {
+    private AccountService pool;
+
+    public LogOut(AccountService pool) {
+        this.pool = pool;
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        pool.logOut(request);
+        JSONObject resp = new JSONObject();
+        try {
+            resp.put("status", "1");
+            resp.put("message", "logout complete");
+        } catch (JSONException e) {
+        }
+        response.getWriter().println(resp.toString());
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
 }
